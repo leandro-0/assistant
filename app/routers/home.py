9b7fabc57ff7583f8c4245f38c1c4c8aa1d9d6e9
Leandro Jiménez <leandroj21p@gmail.com>
2025-02-23 21:58:55 -0400

    chore: templates config

--- app/routers/home.py
+++ app/routers/home.py
@@ -1,9 +1,11 @@
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends, HTTPException, Request
+
+from app.config.templates import templates
 
 
 router = APIRouter(tags=["home"])
 
 
 @router.get("/")
-async def home():
-    return {}
+async def home(request: Request):
+    return templates.TemplateResponse("home.html", {"request": request})
